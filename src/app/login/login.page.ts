e5932fcf69a4f55f09c8335c4e63b87975292b3f
murilabra <mu.labra@duocuc.cl>
2024-10-17 19:40:39 -0300

    Login
    Avance autenticación Login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router } from '@angular/router'; 
+import { Storage } from '@ionic/storage-angular';  
 
 @Component({
   selector: 'app-login',
-  templateUrl: 'login.page.html',
-  styleUrls: ['login.page.scss'],
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
+  username: string = '';  
+  password: string = '';  
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private storage: Storage) {}
 
+<<<<<<< Updated upstream
   // Método para redirigir a la página 'home'
   login() {
     this.router.navigateByUrl('/home');
+=======
+  async login() {
+    if (this.username === 'admin' && this.password === 'admin123') {
+  
+      await this.storage.set('isLoggedIn', true);  
+      this.router.navigate(['/home']);  
+    } else {
+      
+      alert('Credenciales incorrectas. Inténtalo nuevamente.');
+    }
+>>>>>>> Stashed changes
   }
 }
