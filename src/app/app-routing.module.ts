c2c406f4b8acc912e694354c8faa73051dbee3c9
murilabra <mu.labra@duocuc.cl>
2024-10-22 00:31:00 -0300

    Login y autenticación del usuario en aplicación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
-  },  {
+  },
+  {
     path: 'reset-password',
     loadChildren: () => import('./reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
   },
